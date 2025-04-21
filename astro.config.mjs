21103617d67144078b22b6f81e7b74b83e5792bc
Beacon <betterwithbeacon@gmail.com>
2025-04-20 23:09:15 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com", // ✅ Change to your real domain later
   integrations: [mdx(), sitemap(), tailwind()],
-  adapter: cloudflare({
+  adapter: cloudflare(),
+  vite: {
+    css: {
+      postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')]
     platformProxy: {
       enabled: true,
     },
