4c60ee2d9315ddc5cc5d63bbc1f86e55652c4eb3
Beacon <betterwithbeacon@gmail.com>
2025-04-20 20:42:02 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Beacon";
+export const SITE_DESCRIPTION = "Optimize your home service operations with Beacon, your expert partner for ServiceTitan, Housecall Pro, and FSM software implementation. Boost productivity, streamline workflows, and increase profitability today.";
